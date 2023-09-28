a1cc15bf3b7d51bffc89a958f8c35cab1ed945b3
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-09-28 09:51:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ SUPPORT_CHAT_ID = -1001586942391
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://480p:encode@cluster0.7fgwrif.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "PIRO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
